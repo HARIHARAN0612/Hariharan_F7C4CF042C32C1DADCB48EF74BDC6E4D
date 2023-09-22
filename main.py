a966c2e47bdb0a82fc302f789e58ed345ec89490
HARIHARAN0612 <144324905+HARIHARAN0612@users.noreply.github.com>
2023-09-22 17:18:31 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-number=int(input("Enter a value:"))
-res=fact_rec(number)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            return f"Deposited ${amount}. New balance: ${self.__account_balance}"
+        else:
+            return "Deposit amount must be greater than zero."
 
-print("The factorial of {} is {}.".format(number,res))
+    def withdraw(self, amount):
+        if amount > 0 and self.__account_balance >= amount:
+            self.__account_balance -= amount
+            return f"Withdrew ${amount}. New balance: ${self.__account_balance}"
+        elif amount <= 0:
+            return "Withdrawal amount must be greater than zero."
+        else:
+            return "Insufficient funds for withdrawal."
+
+    def display_balance(self):
+        return f"Account balance for {self.__account_holder_name}: ${self.__account_balance}"
+
+
+my_account = BankAccount("12345", "John Doe", 1000.0)
+
+
+print(my_account.display_balance())
+print(my_account.deposit(500))
+print(my_account.withdraw(200))
+print(my_account.withdraw(1500))
